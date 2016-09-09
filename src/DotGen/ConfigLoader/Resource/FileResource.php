fbe042201a17a0fa5d2783a9c5fdf03495b642f2
Cedric Haase <cedric@sineband.de>
2016-09-09 15:42:45 +0200

    Implement FileResource

--- src/DotGen/ConfigLoader/Resource/FileResource.php
+++ src/DotGen/ConfigLoader/Resource/FileResource.php
@@ -1,15 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cedric
- * Date: 9/9/16
- * Time: 3:19 PM
- */
-
 namespace DotGen\ConfigLoader\Resource;
+use DotGen\File\GuessesFileTypeTrait;
+use DotGen\File\HandlesFilesystemTrait;
 
-
-class FileResource
+/**
+ * Class FileResource
+ *
+ * @package DotGen\ConfigLoader\Resource
+ */
+class FileResource implements ResourceInterface
 {
+    use HandlesFilesystemTrait;
+    use GuessesFileTypeTrait;
 
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * FileResource constructor.
+     * 
+     * @param string $path
+     */
+    public function __construct(string $path)
+    {
+        self::assertFileExists($path);
+
+        $this->path = $path;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @see FileType
+     *
+     * @return string
+     */
+    public function getFileType(): string
+    {
+        return self::guessFileType($this->path);
+    }
 }
