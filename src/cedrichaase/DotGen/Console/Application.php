2c9f7844f43ea5a03ad1e07d7e81d7ca610560b0
Cedric Haase <cedric@sineband.de>
2016-09-09 02:12:52 +0200

    Move application initialization to App class

--- src/cedrichaase/DotGen/Console/Application.php
+++ src/cedrichaase/DotGen/Console/Application.php
@@ -1,9 +1,34 @@
 <?php
 namespace cedrichaase\DotGen\Console;
 
+use cedrichaase\DotGen\Console\Command\RenderCommand;
 use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Command\Command;
 
+/**
+ * Class Application
+ *
+ * @package cedrichaase\DotGen\Console
+ */
 class Application extends BaseApplication
 {
-    
+    /**
+     * Application constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('dotgen', '0.1.1');
+
+        $this->addCommands($this->commands());
+    }
+
+    /**
+     * @return Command[]
+     */
+    private function commands()
+    {
+        return [
+            new RenderCommand(),
+        ];
+    }
 }
