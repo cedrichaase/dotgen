522d14266b92f7d52c1e7d718168a2e6cdf19f49
Cedric Haase <cedric@sineband.de>
2016-09-09 15:45:26 +0200

    Implement ConfigLoaderFactory::createFromResource

--- src/DotGen/ConfigLoader/ConfigLoaderFactory.php
+++ src/DotGen/ConfigLoader/ConfigLoaderFactory.php
@@ -1,9 +1,13 @@
 <?php
 namespace DotGen\ConfigLoader;
 
+use DotGen\ConfigLoader\Resource\FileResource;
+use DotGen\ConfigLoader\Resource\ResourceInterface;
+use DotGen\ConfigLoader\Resource\UnknownResourceTypeException;
 use DotGen\File\GuessesFileTypeTrait;
 use DotGen\File\UnknownFileTypeException;
 use DotGen\File\FileType;
+use Symfony\Component\HttpFoundation\File\File;
 
 /**
  * Class ConfigLoaderFactory
@@ -23,19 +27,38 @@ class ConfigLoaderFactory
      * @throws ConfigLoaderException
      * @throws UnknownFileTypeException
      */
-    public static function createFromFile($filePath)
+    public static function createFromFileResource(FileResource $resource)
     {
-        $type = self::guessFileType($filePath);
+        $type = $resource->getFileType();
+        $path = $resource->getPath();
         
         switch($type)
         {
             case FileType::FILE_TYPE_INI:
-                $loader = new IniConfigLoader($filePath);
+                $loader = new IniConfigLoader($path);
                 break;
             default:
                 throw new ConfigLoaderException('No config loader found for file of type ' . $type);
         }
         
         return $loader;
+    }
+
+    /**
+     * @param ResourceInterface $resource
+     *
+     * @return ConfigLoaderInterface
+     *
+     * @throws ConfigLoaderException
+     * @throws UnknownResourceTypeException
+     */
+    public static function createFromResource(ResourceInterface $resource)
+    {
+        if($resource instanceof FileResource)
+        {
+            return self::createFromFileResource($resource);
+        }
+        
+        throw new UnknownResourceTypeException('Creating ConfigLoader from resource of following type is not implemented: ' . get_class($resource));
     }
 }
