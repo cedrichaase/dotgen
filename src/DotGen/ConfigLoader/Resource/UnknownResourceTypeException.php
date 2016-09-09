cc1a96561d4432d0d4cc93b3c59d7695c14a1e56
Cedric Haase <cedric@sineband.de>
2016-09-09 15:44:59 +0200

    Add UnknownResourceTypeException

--- src/DotGen/ConfigLoader/Resource/UnknownResourceTypeException.php
+++ src/DotGen/ConfigLoader/Resource/UnknownResourceTypeException.php
@@ -1,15 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cedric
- * Date: 9/9/16
- * Time: 3:29 PM
- */
-
 namespace DotGen\ConfigLoader\Resource;
 
-
-class UnknownResourceTypeException
+class UnknownResourceTypeException extends \Exception
 {
-
+    // no-op
 }
