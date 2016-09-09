dac841338fc8a4a2095f1b743cadf24c0a89c416
Cedric Haase <cedric@sineband.de>
2016-09-09 02:09:17 +0200

    Wrap up console application

--- src/cedrichaase/cli.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cedric
- * Date: 9/9/16
- * Time: 1:46 AM
- */
--- src/generator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once __DIR__ . '/../vendor/autoload.php';
-
-use cedrichaase\DotGen\DotGen;
-
-$gen = new DotGen($argv[1]);
-$gen->generate();
