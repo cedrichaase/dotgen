968b57221bb258a6971cfff088aafc165a43e2d0
Cedric Haase <cedric@sineband.de>
2016-09-09 15:40:05 +0200

    Reflect namespace changes in composer.json

---
+++ src/DotGen/ConfigLoader/Resource/ResourceInterface.php
@@ -0,0 +1,7 @@
+<?php
+namespace DotGen\ConfigLoader;
+
+interface ResourceInterface
+{
+    
+}
---
+++ src/DotGen/ConfigLoader/Resource/UnknownResourceTypeException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cedric
+ * Date: 9/9/16
+ * Time: 3:29 PM
+ */
+
+namespace DotGen\ConfigLoader\Resource;
+
+
+class UnknownResourceTypeException
+{
+
+}
