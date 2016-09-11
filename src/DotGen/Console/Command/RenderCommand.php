d5ff62bb68d111dfca29a96fbcaf9a27eac464b7
Cedric Haase <cedric@sineband.de>
2016-09-11 18:08:22 +0200

    Write rendered files to fs in render command for now

--- src/DotGen/Console/Command/RenderCommand.php
+++ src/DotGen/Console/Command/RenderCommand.php
@@ -3,6 +3,7 @@ namespace DotGen\Console\Command;
 
 use DotGen\Config\IniStringToArrayConverter;
 use DotGen\DotGen;
+use DotGen\File\HandlesFilesystemTrait;
 use Monolog\Logger;
 use Symfony\Bridge\Monolog\Handler\ConsoleHandler;
 use Symfony\Component\Console\Command\Command;
@@ -12,6 +13,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RenderCommand extends Command
 {
+    use HandlesFilesystemTrait;
+
     /**
      * Command name
      */
@@ -56,7 +59,10 @@ class RenderCommand extends Command
 
         $ini = file_get_contents($path);
         $converter = new IniStringToArrayConverter();
+        $converter->setBasePath(dirname($path));
         $resource = $converter->convert($ini);
+
+
         $dotgen = new DotGen($resource);
 
         $verbose  = $input->getOption(self::OPT_VERBOSE);
@@ -67,6 +73,10 @@ class RenderCommand extends Command
             $dotgen->setLogger($logger);
         }
 
-        $dotgen->render();
+        $renderedFiles = $dotgen->render();
+        foreach($renderedFiles as $i => $renderedFile)
+        {
+            file_put_contents($renderedFile->getDestinationPath(), $renderedFile->getContents());
+        }
     }
 }
