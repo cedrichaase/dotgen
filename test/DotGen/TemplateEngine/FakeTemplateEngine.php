cd334b052688f6dc825bbc0db99c4896be45d0c7
Cedric Haase <cedric@sineband.de>
2016-09-15 03:43:06 +0200

    Add fake template engine

---
+++ test/DotGen/TemplateEngine/FakeTemplateEngine.php
@@ -0,0 +1,36 @@
+<?php
+namespace DotGen\TemplateEngine;
+
+class FakeTemplateEngine implements TemplateEngineInterface
+{
+    const FILE_EXTENSION = 'fake';
+
+    const CONTENT = 'fake rendered content';
+
+    /**
+     * Render the template at given relative $path
+     * with provided $data
+     *
+     * Return the rendered text string
+     *
+     * @param string $path
+     * @param array $data
+     *
+     * @return string
+     */
+    public function render(string $path, array $data): string
+    {
+        return self::CONTENT;
+    }
+
+    /**
+     * Returns the file extension for files that
+     * are written for this templating engine
+     *
+     * @return string
+     */
+    public function getFileExtension(): string
+    {
+        return self::FILE_EXTENSION;
+    }
+}
