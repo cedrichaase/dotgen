e6df11a4fed2dbc2c63c6421fe55075dcc1f9cb7
Cedric Haase <cedric@sineband.de>
2016-09-15 14:35:18 +0200

    Implement TemplateEngineManager

---
+++ src/DotGen/TemplateEngine/TemplateEngineManager.php
@@ -0,0 +1,72 @@
+<?php
+namespace DotGen\TemplateEngine;
+
+/**
+ * Class TemplateEngineManager
+ *
+ * @package DotGen\TemplateEngine
+ */
+class TemplateEngineManager implements ITemplateEngineManager
+{
+    /**
+     * @var ITemplateEngine[]
+     */
+    private $engines;
+
+    /**
+     * TemplateEngineManager constructor.
+     *
+     * @param string $templateDir
+     */
+    public function __construct(string $templateDir)
+    {
+        $this->engines = [];
+        $this->registerEngine(new TwigTemplateEngine($templateDir));
+    }
+
+    /**
+     * Render a template
+     *
+     * @param string $template
+     * @param array $content
+     *
+     * @return string
+     * @throws TemplateEngineException
+     */
+    public function render(string $template, array $content)
+    {
+        foreach($this->engines as $engine)
+        {
+            if($engine->supports($template))
+            {
+                return $engine->render($template, $content);
+            }
+        }
+
+        throw new TemplateEngineException('Do not know how to render template ' . $template);
+    }
+
+    /**
+     * Register a template engine with this Manager.
+     * If a template is supported by multiple Engines, the most recently
+     * registered one will take precedence.
+     *
+     * @param ITemplateEngine $engine
+     */
+    public function registerEngine(ITemplateEngine $engine)
+    {
+        array_unshift($this->engines, $engine);
+    }
+
+    /**
+     * Exclusively register one template engine
+     *
+     * @param ITemplateEngine $engine
+     */
+    public function registerExclusive(ITemplateEngine $engine)
+    {
+        unset($this->engines);
+        $this->engines = [];
+        $this->registerEngine($engine);
+    }
+}
