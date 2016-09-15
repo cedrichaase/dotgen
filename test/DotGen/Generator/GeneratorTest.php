462b8a2654537ac0b8baf9cd7791cc719870eea3
Cedric Haase <cedric@sineband.de>
2016-09-15 03:43:20 +0200

    Add GeneratorTest

---
+++ test/DotGen/Generator/GeneratorTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace DotGen\Generator;
+
+use DotGen\Config\Entity\Collection;
+use DotGen\Config\Resource\FakeResource;
+use DotGen\TemplateEngine\FakeTemplateEngine;
+
+class GeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRender()
+    {
+        // arrange
+        $engine = new FakeTemplateEngine();
+        $resource = new FakeResource();
+
+        $firstTemplateNames = ['a', 'b', 'c'];
+        $secondTemplateNames = ['h', 'j', 'k'];
+        $allTemplateNames = array_merge($firstTemplateNames, $secondTemplateNames);
+
+        $firstCollection = new Collection('my_collection', ['a' => 'b'], $firstTemplateNames);
+        $secondCollection = new Collection('my_2nd_collection', ['x' => 'y'], $secondTemplateNames);
+        $resource->setTemplatePath('/template/path');
+        $resource->setCollections([$firstCollection, $secondCollection]);
+
+        $generator = new Generator($resource, $engine);
+
+        // act
+        $renderedFiles = $generator->render();
+
+        // assert
+        self::assertSame(count($renderedFiles), 6);
+
+        foreach($renderedFiles as $i => $renderedFile)
+        {
+            self::assertSame($renderedFile->getTemplateName(), $allTemplateNames[$i]);
+            self::assertSame($renderedFile->getContents(), FakeTemplateEngine::CONTENT);
+        }
+    }
+}
