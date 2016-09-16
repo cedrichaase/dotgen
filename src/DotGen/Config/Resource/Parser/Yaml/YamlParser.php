d176e6e7fee9d8ffe7a35e24e3e61875a1f6c97d
Cedric Haase <cedric@sineband.de>
2016-09-16 03:30:34 +0200

    Implement YamlParser

---
+++ src/DotGen/Config/Resource/Parser/Yaml/YamlParser.php
@@ -0,0 +1,43 @@
+<?php
+namespace DotGen\Config\Resource\Parser\Yaml;
+
+use DotGen\Config\Resource\Parser\IParser;
+use DotGen\Config\Resource\Parser\ParserException;
+use Symfony\Component\Yaml\Yaml;
+
+class YamlParser implements IParser
+{
+    /**
+     * Parse the given string to an array
+     *
+     * @param string $string
+     * @return array
+     *
+     * @throws ParserException
+     */
+    public function parse(string $string): array
+    {
+        return Yaml::parse($string);
+    }
+
+    /**
+     * Determine whether or not the given string
+     * can be parsed by this parser
+     *
+     * @param string $string
+     * @return bool
+     */
+    public function supports(string $string): bool
+    {
+        $supports = true;
+
+        try {
+            Yaml::parse($string);
+        } catch (ParserException $e)
+        {
+            $supports = false;
+        }
+
+        return $supports;
+    }
+}
---
+++ src/DotGen/Config/Resource/Parser/Yaml/YamlParserException.php
@@ -0,0 +1,9 @@
+<?php
+namespace DotGen\Config\Resource\Parser\Yaml;
+
+use DotGen\Config\Resource\Parser\ParserException;
+
+class YamlParserException extends ParserException
+{
+    // no-op
+}
