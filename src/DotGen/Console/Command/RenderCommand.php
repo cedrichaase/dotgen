4d43eaab71350d2be12470e965619e454a2164c1
Cedric Haase <cedric@sineband.de>
2016-09-17 17:13:00 +0200

    Add inheritance and some time measurements to console app

--- src/DotGen/Console/Command/RenderCommand.php
+++ src/DotGen/Console/Command/RenderCommand.php
@@ -3,7 +3,9 @@ namespace DotGen\Console\Command;
 
 use DotGen\Config\Resource\Converter\ArrayToResourceConverter;
 use DotGen\Config\Resource\Converter\BaseDirTemplateMapper;
+use DotGen\Config\Resource\InheritanceHandler\InheritanceHandler;
 use DotGen\Config\Resource\Parser\ParserManager;
+use DotGen\Config\Resource\Repository\File\FileResourceRepository;
 use DotGen\File\HandlesFilesystemTrait;
 use DotGen\Generator\Generator;
 use DotGen\TemplateEngine\TemplateEngineManager;
@@ -52,6 +54,13 @@ class RenderCommand extends Command
     const OPT_OUTPUT_PATH_SHORT = 'o';
 
     /**
+     * resource repository include option
+     */
+    const OPT_INCLUDE = 'include';
+
+    const OPT_INCLUDE_SHORT = 'I';
+
+    /**
      * verbose option
      */
     const OPT_VERBOSE = 'verbose';
@@ -81,6 +90,15 @@ class RenderCommand extends Command
             InputOption::VALUE_OPTIONAL,
             'output path for rendered files (defaults to config file path)'
         );
+
+        $this->addOption(
+            self::OPT_INCLUDE,
+            self::OPT_INCLUDE_SHORT,
+            InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+            "Path to a resource include.
+            Right now, these can only be paths to directories containing config files prefixed with \"file:\"
+            Config file path is always included."
+        );
     }
 
     /**
@@ -91,6 +109,8 @@ class RenderCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $startExecute = microtime(true);
+
         $path = $input->getArgument(self::ARG_CONFIG_FILE);
         if(!$path || !file_exists($path))
         {
@@ -109,6 +129,19 @@ class RenderCommand extends Command
             $outputDir = realpath(dirname($path));
         }
 
+        // process include option
+        $includes = $input->getOption(self::OPT_INCLUDE);
+        $includes[] = realpath(dirname($path));
+        // remove file: prefix from paths
+        $includes = array_map(function($element) {
+            if(substr($element, 0, strlen('file:')) === 'file:')
+            {
+                $element = substr($element, strlen('file:'));
+            }
+
+            return realpath($element);
+        }, $includes);
+
         $verbose  = $input->getOption(self::OPT_VERBOSE);
         $logger = new NullLogger();
         if($verbose)
@@ -121,10 +154,27 @@ class RenderCommand extends Command
         $parser = new ParserManager();
         $parsed = $parser->parse($config);
 
+        // convert parsed array to resource
         $converter = new ArrayToResourceConverter();
         $converter->setLogger($logger);
         $resource = $converter->convert($parsed);
 
+
+        $startInheritance = microtime(true);
+        // handle inheritance
+        $handler = new InheritanceHandler();
+        foreach($includes as $include)
+        {
+            $repo = new FileResourceRepository($include);
+            $handler->registerRepository($repo);
+        }
+        $resource = $handler->extend($resource);
+
+        $logger->info('Done handling inheritance', [
+            'time' => microtime(true) - $startInheritance,
+        ]);
+
+        // begin render process
         $engine = new TemplateEngineManager($templateDir);
 
         $generator = new Generator($resource, $engine);
@@ -138,6 +188,10 @@ class RenderCommand extends Command
             $renderedFilePath = $mapper->map($renderedFile->getTemplateName());
             file_put_contents($renderedFilePath, $renderedFile->getContents());
         }
+
+        $logger->info('Finished execution', [
+            'time' => microtime(true) - $startExecute,
+        ]);
 
         return;
     }
