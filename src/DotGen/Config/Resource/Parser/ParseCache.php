cecf2624d8b85b8e3d85cb0890a809299b26cdb6
Cedric Haase <cedric@sineband.de>
2016-09-18 03:36:15 +0200

    Add ParseCache

---
+++ src/DotGen/Config/Resource/Parser/ParseCache.php
@@ -0,0 +1,50 @@
+<?php
+namespace DotGen\Config\Resource\Parser;
+
+/**
+ * Class ParseCache
+ *
+ * @package DotGen\Config\Resource\Parser
+ */
+class ParseCache
+{
+    /**
+     * @var array
+     */
+    private $cache;
+
+    /**
+     * ParseCache constructor.
+     */
+    public function __construct()
+    {
+        $this->cache = [];
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return array
+     */
+    public function find(string $string): array
+    {
+        return ($this->cache[sha1($string)]) ?? [];
+    }
+
+    /**
+     * @param string $string
+     *
+     * @param array $parsed
+     */
+    public function save(string $string, array $parsed)
+    {
+        $this->cache[sha1($string)] = $parsed;
+    }
+
+    public function tryCache($parseFunction, $string)
+    {
+        if(!$parsed = $this->find($string))
+            $parsed = $parseFunction($string);
+        return $parsed;
+    }
+}
