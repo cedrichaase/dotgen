ec7c3ca2f407cd41e8700d2c6c76ba60cafe31e8
Cedric Haase <cedric@sineband.de>
2016-09-20 23:23:52 +0200

    Add PhpColorsExtension

---
+++ src/DotGen/Twig/PhpColorsExtension.php
@@ -0,0 +1,67 @@
+<?php
+namespace DotGen\Twig;
+
+use Mexitek\PHPColors\Color;
+
+/**
+ * Class PhpColorsExtension
+ *
+ * @package DotGen\Twig
+ */
+class PhpColorsExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('darken', [$this, 'darken']),
+            new \Twig_SimpleFilter('lighten', [$this, 'lighten']),
+            new \Twig_SimpleFilter('saturate', [$this, 'saturate']),
+            new \Twig_SimpleFilter('desaturate', [$this, 'desaturate']),
+            new \Twig_SimpleFilter('complementary', [$this, 'complementary']),
+        ];
+    }
+
+    public function darken($colorString, $amount = 10)
+    {
+        $color = new Color($colorString);
+        return $color->darken($amount);
+    }
+
+    public function lighten($colorString, $amount = 10)
+    {
+        $color = new Color($colorString);
+        return $color->lighten($amount);
+    }
+
+    public function complementary($colorString)
+    {
+        $color = new Color($colorString);
+        return $color->complementary();
+    }
+
+    public function saturate($colorString, $amount = 10)
+    {
+        $hsl = Color::hexToHsl($colorString);
+        $saturation = $hsl['S'];
+        $hsl['S'] = $saturation + min($saturation * $amount / 100, 1);
+        return Color::hslToHex($hsl);
+    }
+
+    public function desaturate($colorString, $amount = 10)
+    {
+        $hsl = Color::hexToHsl($colorString);
+        $saturation = $hsl['S'];
+        $hsl['S'] = $saturation - min($saturation * $amount / 100, 1);
+        return Color::hslToHex($hsl);
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'phpcolors';
+    }
+}
